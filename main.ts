1d535b08353cc5a9d0d45c83069c32c6be7aff23
GerbH57 <92798943+GerbH57@users.noreply.github.com>
2021-11-08 09:35:12 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,50 @@
-let Dice3 = 0
-let Dice1 = 0
-let Dice5 = 0
-let Dice2 = 0
-let Dice4 = 0
 input.onPinPressed(TouchPin.P0, function () {
-    let Dice3R2 = 0
-    Dice3 = Dice3R2
+    if (Dice3R2 > 0) {
+        Dice3R2 = 0
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
+    }
 })
 input.onButtonPressed(Button.A, function () {
-    let Dice1R2 = 0
     Dice1 = Dice1R2
 })
 input.onPinPressed(TouchPin.P2, function () {
-    let Dice5R2 = 0
     Dice5 = Dice5R2
 })
 input.onButtonPressed(Button.B, function () {
-    let Dice2R2 = 0
-    Dice2 = Dice2R2
+    if (Dice3R2 > 0) {
+        Dice3R2 = 0
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
+    }
 })
 input.onPinPressed(TouchPin.P1, function () {
-    let Dice4R2 = 0
-    Dice4 = Dice4R2
+    if (Dice4R2 > 0) {
+        Dice4R2 = 0
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
+    }
 })
 input.onGesture(Gesture.Shake, function () {
+    shakecount = shakecount + 1
     Dice1 = randint(1, 6)
     basic.showString("" + (Dice1))
     basic.pause(1000)
@@ -39,4 +60,22 @@ input.onGesture(Gesture.Shake, function () {
     Dice5 = randint(1, 6)
     basic.showString("" + (Dice5))
     basic.pause(1000)
+})
+let Dice4 = 0
+let Dice3 = 0
+let Dice2 = 0
+let shakecount = 0
+let Dice5 = 0
+let Dice1 = 0
+let Dice5R2 = 0
+let Dice4R2 = 0
+let Dice3R2 = 0
+let Dice1R2 = 0
+Dice1R2 = randint(1, 6)
+let Dice2R2 = randint(1, 6)
+Dice3R2 = randint(1, 6)
+Dice4R2 = randint(1, 6)
+Dice5R2 = randint(1, 6)
+basic.forever(function () {
+	
 })
